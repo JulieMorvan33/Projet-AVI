cce0b78e04e78b1e5560f4ac2a591e554dc44138
JohMey33 <meyjoh33@gmail.com>
2021-01-27 16:42:51 +0100

    Modification durant la séance Intégration du 26 janvier

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import ndWindowParameters
 import time
 
 USE_IVY = True  # use or not use Ivy Bus ?
-AC_SIMULATED = True  # use bus IVY with no other groups ?
+AC_SIMULATED = False  # use bus IVY with no other groups ?
 SIMU_DELAY = 0.1  # increment time for the simulation if Ivy Bus isn't used
 
 def null_cb(*a):
