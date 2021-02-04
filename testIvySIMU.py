2e4bb76b2519d650eac2859b9d5f5725ef01fd08
JulieMorvan33 <45207107+JulieMorvan33@users.noreply.github.com>
2021-02-04 13:20:36 +0100

    Commentaires testIvySIMU

--- testIvySIMU.py
+++ testIvySIMU.py
@@ -19,17 +19,15 @@ if __name__ == '__main__':
 
     # Envoie du temps
     IvySendMsg("GC_AP Time=0 AP_State='HDG'")  # message pas pris en compte
-    IvySendMsg("FCULateral Mode=SelectedHeading Val=150")
+    IvySendMsg("FCULateral Mode=SelectedHeading Val=150")  # mode heading envoyé par le simu
     time.sleep(1)
     for i in range(50):
         IvySendMsg("Time t="+str(float(i)))
         print("Time t="+str(float(i)))
         x, y = (1861+i*2)*NM2M, (304.5-i)*NM2M  # m
 
-        # if USING_STATE_VECTOR:
         message1 = "StateVector x=" + str(x) + " y=" + str(y) + " z=" + str(30000*FT2M)
         message1 += " Vp=" + str(250*NM2M*3600) + " fpa=" + str(273) + " psi=" + str(0) + " phi=" + str(0)
-        #else:
         message2 = "AircraftSetPosition X=" + str(x) + " Y=" + str(y)
         message2 += " Altitude-ft=" + str(30000*FT2M) + " Roll=" + str(0) + " Pitch=" + str(0) + " Yaw=" + str(0)
         message2 += " Heading=" + str(273) + " Airspeed=" + str(250) + " Groundspeed=" + str(265)
@@ -47,10 +45,8 @@ if __name__ == '__main__':
         IvySendMsg("Time t=" + str(float(i)))
         x, y = (100 + i * 0.1)*100*NM2M, (75 + i * 1.5/20)*100*NM2M   # m
 
-        #if USING_STATE_VECTOR:
         message1 = "InitStateVector x=" + str(x) + " y=" + str(y) + " z=" + str(30000*FT2M)
         message1 += " Vp=" + str(250*NM2M*3600) + " fpa=" + str(273) + " psi=" + str(0) + " phi=" + str(0)
-        #else:
         message2 = "AircraftSetPosition X=" + str(x) + " Y=" + str(y)
         message2 += " Altitude-ft=" + str(30000) + " Roll=" + str(0) + " Pitch=" + str(0) + " Yaw=" + str(0)
         message2 += " Heading=" + str(273) + " Airspeed=" + str(250) + " Groundspeed=" + str(265)
